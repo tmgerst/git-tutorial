38448ace95d5fae048610bf02c9d6d27d2b0e24c
Tim Gerstewitz <t.gerstewitz@ujzega.de>
2020-11-13 18:23:37 +0100

    Cherry-Picked main.patch and patched main.c to fulfil exercise requirements.

--- src/main.c
+++ src/main.c
@@ -2,8 +2,27 @@
 // main.c for Coding Challenge 1 in the ESPL git-tutorial
 
 #include <stdio.h>
+#include "espl_lib.h"
 
 int main(){
-    printf("Hello ESPL\n");
+    unsigned int number = 0;
+    int counter = 0;
+    char* ret_string;
+
+    // Eingabe ent
+    printf("Enter a number between 0 and 9: ");
+    scanf("%i", &number);
+
+    ret_string = num_to_words(number);
+
+    while(ret_string != NULL){
+        printf("%c", *(ret_string + counter));
+        counter++;
+    }
+    printf("\n");
+
+    //printf("Hello ESPL\n");
+    //printf("This is a new print.");
+
     return 0;
 }
